4f35901d13d1931eeb0bef1646ae1b9a36d179d5
sej3939 <sj3939@nyu.edu>
2024-10-11 14:41:32 -0400

    Add files via upload

--- test/tb.v
+++ test/tb.v
@@ -17,9 +17,11 @@ module tb ();
   reg clk;
   reg rst_n;
   reg ena;
-  reg [7:0] ui_in;
+  reg [3:0] a,b;
   reg [7:0] uio_in;
-  wire [7:0] uo_out;
+  wire [3:0] sum;
+  wire carry_out;
+  wire [2:0] uo_dum;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 `ifdef GL_TEST
@@ -28,7 +30,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_koggestone_adder4 user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
@@ -36,8 +38,8 @@ module tb ();
       .VGND(VGND),
 `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
+      .ui_in  ({b,a}),    // Dedicated inputs
+      .uo_out ({uo_dum,carry_out,sum}),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
       .uio_out(uio_out),  // IOs: Output path
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
