277a765ba000effe54268a38e882e313f794ca7e
sej3939 <sj3939@nyu.edu>
2024-10-11 14:42:04 -0400

    Add files via upload

--- test/test.py
+++ test/test.py
@@ -14,27 +14,20 @@ async def test_project(dut):
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.a.value = 13
+    dut.b.value = 10
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 10)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    dut._log.info(f"value of outputs are: {dut.sum.value} and {dut.carry_out.value}.")
+    assert dut.sum.value == 7 and dut.carry_out.value == 1 
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+    
