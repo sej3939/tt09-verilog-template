786bc1a3c80b2a8d018c87cdff3c98bb836fe046
sej3939 <sj3939@nyu.edu>
2024-12-12 19:17:57 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -10,8 +10,8 @@ from cocotb.triggers import ClockCycles
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 0.01 us (100 MHz)
-    clock = Clock(dut.clk, 0.01, units="us")
+    # Set the clock period to 10 us (100 kHz)
+    clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
     dut._log.info("Test project behavior")
